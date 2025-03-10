0b7e6c87916b98649ecc77362914985e6c74da0c
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2025-03-09 22:38:14 -0300

    init front and back

---
+++ easy-time-front/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider"
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "EasyTime",
+  description: "Agendamento rápido e fácil!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt_BR" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
